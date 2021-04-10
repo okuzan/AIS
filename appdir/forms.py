751806e40688db000dd7d5086c9111dc6b8b6cfc
Taisiia Shutiak <taisiia.shutiak@ukma.edu.ua>
2021-04-10 23:16:54 +0300

    add producer adding form

--- appdir/forms.py
+++ appdir/forms.py
@@ -1,7 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, validators
 from wtforms.validators import DataRequired, Email
+
 
 class CategoryForm(FlaskForm):
     name = StringField("Name: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class ProducerForm(FlaskForm):
+    contract_number = StringField("Contract number: ", validators=[DataRequired(message="This field can not be empty"),
+                                             validators.Length(max=15, message="Contract number can not contain more than 15 symbols")])
+    name = StringField("Name: ", validators=[DataRequired(message="This field can not be empty"),
+                                             validators.Length(max=50, message="Producer name can not contain more than 50 symbols")])
+    country = StringField("Country: ", validators=[DataRequired(message="This field can not be empty"),
+                                             validators.Length(max=50, message="Producer name can not contain more than 50 symbols")])
+    city = StringField("City: ", validators=[DataRequired(message="This field can not be empty"),
+                                             validators.Length(max=50, message="Producer name can not contain more than 50 symbols")])
+    street = StringField("Street: ", validators=[DataRequired(message="This field can not be empty"),
+                                             validators.Length(max=50, message="Producer name can not contain more than 50 symbols")])
+    zip_code = StringField("Zip code: ", validators=[DataRequired(message="This field can not be empty"),
+                                                        validators.Regexp(r'(^\d{5}$)|(^\d{9}$)|(^\d{5}-\d{4}$)',
+                                                                          message="Incorrect zip code format, it can be xxxxx or xxxxxxxxx or xxxxx-xxxx (only numbers allowed)")])
+
+    phone_number = StringField("Phone: ", validators=[DataRequired(message="This field can not be empty"),
+                                                        validators.Regexp(r'^\+\d{12}$',
+                                                                          message="Incorrect phone format +xxxxxxxxxxxx")])
+
     submit = SubmitField("Submit")
--- appdir/routes.py
+++ appdir/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template_string, Blueprint, render_template, request, redirect, url_for, flash
 from flask_user import login_required, roles_required
 from flask_script import Manager, Command, Shell
-from forms import CategoryForm
+from forms import CategoryForm, ProducerForm
 
 import sqlite3 as sql
 
@@ -60,8 +60,11 @@ def category():
             con = sql.connect('dbs/zlagoda.db')
             con.row_factory = sql.Row
             cur = con.cursor()
-            cur.execute('''INSERT INTO CATEGORY(CATEGORY_NAME)
-                                   VALUES (?);''', (name,))
+            cur.execute("SELECT MAX(CATEGORY_NUMBER) FROM CATEGORY")
+            result = cur.fetchone()
+            max_id = int(result[0]) + 1
+            cur.execute('''INSERT INTO CATEGORY
+                                   VALUES (?, ?);''', (max_id, name))
             con.commit()
             cur.close()
             flash('Category was successfully added', 'success')
@@ -73,3 +76,29 @@ def category():
             if (con):
                 con.close()
     return render_template('form.html', form=form, title='Add category')
+
+@blueprint.route('/producer/', methods=['get', 'post'])
+@roles_required('Admin')  # Use of @roles_required decorator
+def producer():
+    form = ProducerForm()
+    if form.validate_on_submit():
+        try:
+            con = sql.connect('dbs/zlagoda.db')
+            con.row_factory = sql.Row
+            cur = con.cursor()
+            cur.execute("SELECT MAX(ID_PRODUCER) FROM PRODUCER")
+            result = cur.fetchone()
+            max_id = int(result[0])+1
+            cur.execute('''INSERT INTO PRODUCER(ID_PRODUCER, CONTRACT_NUMBER, RPOD_NAME,COUNTRY, CITY, STREET, ZIP_CODE, PHONE_NUMBER)
+                                   VALUES (?, ?, ?, ?, ?, ?, ?, ?);''', (max_id, form.contract_number.data, form.name.data, form.country.data, form.city.data, form.street.data, form.zip_code.data, form.phone_number.data))
+            con.commit()
+            cur.close()
+            flash('Producer was successfully added', 'success')
+            return redirect(url_for('blueprint.producer'))
+        except sql.Error as error:
+            flash(error, 'danger')
+            return render_template('form.html', form=form, title='Add producer')
+        finally:
+            if (con):
+                con.close()
+    return render_template('form.html', form=form, title='Add producer')

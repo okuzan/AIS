54049e8d0e3b2e9bb623910afe2274b2de2168f5
Oleh Kuzan <kuzanoleg@gmail.com>
2021-04-12 18:18:59 +0300

    ui, cashiers

--- appdir/app.py
+++ appdir/app.py
@@ -2,4 +2,4 @@ from appdir import app
 
 # Start development web server
 if __name__ == '__main__':
-    app.run(host='localhost', port=3001, debug=True)
+    app.run(host='localhost', port=3000, debug=True)
